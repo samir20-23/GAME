905e748bfc4b7743ca3b70c043156af7b20cc31a
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-04-16 16:59:59 +0100

    Add files via upload

---
+++ anime/anime.js
@@ -0,0 +1,8 @@
+let load = document.getElementById("load");
+
+let deathnote = document.getElementById("deathnote");
+    deathnote.addEventListener("click",function(){
+      window.location.replace("deathnote.html");
+      load.style.display="block";
+    });
+    
